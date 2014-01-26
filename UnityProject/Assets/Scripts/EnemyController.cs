c5273455cf0c4d8e4a44b531ee4f39b60973cdf2
Francisco Rimoldi <francisco.rimoldi@toptal.com>
2014-01-26 04:33:21 -0300

    Pelea contra orcas

---
+++ UnityProject/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : CharacterController {
+	
+	public float timeBetweenAttacks = 1;
+
+	private GameObject player;
+	private bool hasToAttack = false;
+	private bool hasToFollow = false;
+	private float timePassed = 0;
+	
+
+	public override void Start() {
+		life = 100;
+		maxSpeed = 3;
+		damage = 5;
+
+		animator = GetComponent<Animator> ();
+		player = GameObject.Find ("UpCharacter");
+	}
+
+	void FixedUpdate() {
+		if (hasToFollow) {
+			float movementToPlayer = Mathf.Sign (player.transform.position.x - transform.position.x) * maxSpeed;
+
+			if ((movementToPlayer > 0 && !facingRight) || (movementToPlayer < 0 && facingRight))
+				Flip();
+
+			rigidbody2D.velocity = new Vector2 (movementToPlayer, rigidbody2D.velocity.y);
+		}
+
+		if (hasToAttack) {
+			if (timePassed >= timeBetweenAttacks) {
+				Attack(player);
+				timePassed = 0;
+			} else {
+				timePassed += Time.fixedDeltaTime;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.name == "UpCharacter") {
+			hasToAttack = true;
+			hasToFollow = false;
+		}
+
+		if (collision.gameObject.tag == "Ground") {
+			hasToFollow = true;
+		}
+
+		if (collision.gameObject.tag == "Enemy") {
+			hasToFollow = false;		
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.name == "UpCharacter") {
+			hasToAttack = false;
+			hasToFollow = true;
+		}
+
+		if (collision.gameObject.tag == "Enemy") {
+			hasToFollow = true;		
+		}
+	}
+
+	protected override void Die() {
+		base.Die ();
+		hasToAttack = false;
+		hasToFollow = false;
+	}
+}
+	
---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : CharacterController {
+	
+	public float jumpForce = 500;
+
+	GameObject enemyToAttack;
+	
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+
+		life = 100;
+		damage = 50;
+		maxSpeed = 10;
+	}
+
+	void FixedUpdate() {
+		if(game.GetComponent<SwitchCharacters>().upSceneActive) {
+			float move = Input.GetAxis ("Horizontal");
+
+			animator.SetFloat ("speed", Mathf.Abs (move));
+
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+			if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+				Flip ();
+
+		}
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Jump")) {
+			rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+		}
+
+		if (Input.GetButtonDown("Fire1")) {
+			Attack(enemyToAttack);
+		}
+	}
+
+	//Colisiones
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Enemy") {
+			enemyToAttack = collision.gameObject;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Enemy") {
+			enemyToAttack = null;
+		}
+	}
+}
